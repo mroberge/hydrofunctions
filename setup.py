ec1913d154177837905bd849ef248d56e36a0be8
mroberge <mroberge.whois@gmail.com>
2016-09-05 23:25:54 -0400

    find_packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, findpackages
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -27,7 +27,7 @@ setup(
     author="Martin Roberge",
     author_email='mroberge.whois@gmail.com',
     url='https://github.com/mroberge/hydrofunctions',
-    packages=findpackages(),
+    packages=find_packages(),
     package_dir={'hydrofunctions':
                  'hydrofunctions'},
     include_package_data=True,
