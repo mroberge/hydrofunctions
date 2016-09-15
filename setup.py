4029ac90b0b8559f8848639762757985bd17269e
mroberge <mroberge.whois@gmail.com>
2016-09-15 11:27:56 -0400

    updated version# to v1.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='hydrofunctions',
-    version='0.1.2',
+    version='0.1.3',
     description="A suite of convenience functions for exploring water data in IPython.",
     long_description=readme + '\n\n' + history,
     author="Martin Roberge",
