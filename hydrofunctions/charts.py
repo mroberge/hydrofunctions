8313ec18b86c11c869eea413c2b5ecfe8dc222b2
mroberge <mroberge.whois@gmail.com>
2018-03-09 10:41:42 -0500

    new module and test_module

---
+++ hydrofunctions/charts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+charts.py
+
+Charting functions for Hydrofunctions.
+"""
+
---
+++ tests/test_charts.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+test_charts.py
+
+Tests for the charts.py module.
+"""
+
