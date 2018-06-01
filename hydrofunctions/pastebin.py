cdb3f4c10a43b7b67c491c774e994accbb6e225a
mroberge <mroberge.whois@gmail.com>
2018-06-01 11:07:19 -0400

    Create a paste bin for new code snippets
    **pastebin.py** is not meant to be included in any merge with Develop!
    As the name implies, this is just a dump where I can store useful code
    from my notebooks in one central location for later development.
    
    1) Paste new code from the notebooks into this module.
    
    2) If I choose to develop some code further:
    - move it into an official module
    - add tests
    - add documentation & example notebooks
    ==> OPTION 1: create a branch off of paste-bin with a descriptive name
    - delete this module (pastebin.py)
    - merge the new branch with the Develop branch
    - Keep adding stuff to the paste-bin branch; repeat
    ==> OPTION 2: Create a branch off of Develop
    - Cut and paste new files and modules from paste-bin branch into the new
    branch
    - Keep adding stuff to the paste-bin branch; repeat

---
+++ hydrofunctions/pastebin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 10:45:29 2018
+
+@author: Marty
+
+pastebin.py
+
+This file is not meant to be included in any merge with Develop!
+As the name implies, this is just a dump where I can store useful code
+from my notebooks in one central location for later development.
+
+1) Paste new code from the notebooks into this module.
+
+2) If I choose to develop some code further:
+    - move it into an official module
+    - add tests
+    - add documentation & example notebooks
+    ==> OPTION 1: create a branch off of paste-bin with a descriptive name
+            - delete this module (pastebin.py)
+            - merge the new branch with the Develop branch
+            - Keep adding stuff to the paste-bin branch; repeat
+    ==> OPTION 2: Create a branch off of Develop
+            - Cut and paste new files and modules from paste-bin branch into the new branch
+            - Keep adding stuff to the paste-bin branch; repeat
+"""
+
