32268353d68c9395a65632318ccd54ac8da8da80
mroberge <mroberge.whois@gmail.com>
2019-05-01 20:27:02 -0400

    Update conf.py
    get napoleon to work on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,13 @@ import hydrofunctions
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosectionlabel', 'sphinxcontrib.napoleon']
+extensions = [
+        'sphinx.ext.autodoc',
+        'sphinx.ext.viewcode',
+        'sphinx.ext.autosectionlabel',
+        'sphinxcontrib.napoleon',
+        'sphinx.ext.napoleon',
+        ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
