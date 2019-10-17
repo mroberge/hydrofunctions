4b9b309bae2d781b1504c280678656a8fff0bfd5
mroberge <mroberge.whois@gmail.com>
2019-10-16 22:58:52 -0400

    update station.py docstring

--- hydrofunctions/station.py
+++ hydrofunctions/station.py
@@ -80,7 +80,7 @@ class NWIS(Station):
             A valid code can also be given as a list: parameterCd=['00060','00065']
             This will request data for this parameter.
 
-                * if value is 'all', or no value is submitted, then NWIS will
+                * if value is 'all', or no value is submitted, then NWIS will \
                 return every parameter collected at this site. (default option)
                 * stage: '00065'
                 * discharge: '00060'
@@ -89,7 +89,7 @@ class NWIS(Station):
 
         period (str):
             NWIS period code. Default is None.
-                * Format is "PxxD", where xx is the number of days before
+                * Format is "PxxD", where xx is the number of days before \
                 today, with a maximum of 999 days accepted.
                 * Either use start_date or period, but not both.
 
