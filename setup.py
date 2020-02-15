5994410c08e5e831091df8651d536e02fb9cee8f
mroberge <mroberge.whois@gmail.com>
2020-02-14 22:34:44 -0500

    require numpy above 1.16.0
    pyarrow problems

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ readme = relative2absolute(readme, relative, stem)
 
 requirements = [
     'matplotlib',
-    'numpy>=1.14.0',
+    'numpy>=1.16.0',
     'pandas',
     'requests',
     'IPython',
