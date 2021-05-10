7a37956c6c058d3273ed868f26810b6a194734a5
elmerehbi <elmerehbi@users.noreply.github.com>
2021-05-11 00:33:39 +0300

    fixed unit tests

--- tests/test_waterwatch.py
+++ tests/test_waterwatch.py
@@ -1,56 +1,26 @@
+import json
 import unittest
 from unittest import mock
-from .fixtures import (
-    fakeResponse,
-)
-from hydrofunctions.waterwatch import get_flood_stage
+
+import hydrofunctions.waterwatch as ww
 
 
 class TestFloodStage(unittest.TestCase):
     def test_correct_filtering(self):
-        stages = {
-            "07144100": {
-                "action_stage": "20",
-                "flood_stage": "22",
-                "moderate_flood_stage": "25",
-            }
-        }
-        expected = {
-            "07144100": {
-                "action_stage": "20",
-                "flood_stage": "22",
-                "moderate_flood_stage": "25",
-            },
-            "07144101": None,
-        }
+        stages = {"07144100": {"action_stage": "20"}}
+        expected = {"07144100": {"action_stage": "20"}, "07144101": None}
         stations = ["07144100", "07144101"]
-        self.assertDictEqual(get_flood_stage(stages, stations), expected)
+        self.assertDictEqual(ww._get_flood_stage(stages, stations), expected)
 
-    @mock.patch("requests.get")
-    def test_waterwatch_get_stage(self, mock_get):
+    @mock.patch("hydrofunctions.waterwatch.__get_flood_stages")
+    def test_waterwatch_get_stage(self, mock_get_fs):
         site = "07144100"
-        expected_format = "json"
+        expected = {site: {'action_stage': '20', 'flood_stage': '22', 'moderate_flood_stage': '25'}}
+        mock_get_fs.content = json.dumps({'sites': [{'site_no': site, 'action_stage': '20', 'flood_stage': '22', 'moderate_flood_stage': '25'}]},separators=(',', ':')).encode('utf-8')
+        mock_get_fs.return_value = expected
+        result = mock_get_fs(site=site)
+        self.assertEqual(result, expected)
 
-        expected_url = "https://waterwatch.usgs.gov/webservices/floodstage"
-        expected_headers = {"Accept-encoding": "gzip"}  #We should make sure to set this header, especially if we request all of the stations! (but it might be automatic)
-        expected_params = {
-            "format": expected_format,
-            "sites": site,
-        }
-        expected = fakeResponse()  # I created 'fakeResponse' so we can have a mock request object to play with
-        expected.json = {
-            "07144100": {
-                "action_stage": "20",
-                "flood_stage": "22",
-                "moderate_flood_stage": "25",
-            }
-        }
 
-        mock_get.return_value = expected  # when we call requests, we want it to return a request object.
-        actual = hf.get_flood_stage(site)      # finally, we get to the actual test!
-        mock_get.assert_called_once_with(   # This is the important test: did the function call Requests properly?
-            expected_url, params=expected_params, headers=expected_headers
-        )
-        self.assertEqual(actual, expected.json)
 if __name__ == "__main__":
     unittest.main(verbosity=2)
